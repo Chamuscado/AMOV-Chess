92feb85c62584b18122b6da1cbf1684e76181784
Chamuscado <32516659+Chamuscado@users.noreply.github.com>
2017-12-01 19:00:19 +0000

    Drawable adicionados
    Drawable adicionados

---
+++ app/src/main/java/com/isec/alex_joao/amov_tp/StartActivity.java
@@ -0,0 +1,25 @@
+package com.isec.alex_joao.amov_tp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void onNewGame(View view) {
+        Toast.makeText(getApplicationContext(),getString(R.string.notDoneYet),Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onLoadGame(View view) {
+        Toast.makeText(getApplicationContext(),getString(R.string.notDoneYet),Toast.LENGTH_SHORT).show();
+    }
+}
